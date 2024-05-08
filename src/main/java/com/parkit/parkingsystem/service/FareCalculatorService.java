111cc18ddb93764c2bfc78f14406192244ace130
Michelle Mairet <michelle.rk.mairet@gmail.com>
2024-05-08 12:53:26 +0200

    Résolution bug calcul prix

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,27 +5,37 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
-        }
+	public void calculateFare(Ticket ticket) {
+		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
+			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
+		}
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+		// Obtenir le temps en millisecondes (comme dans FareCalculatorServiceTest.java)
+		long inTimeInMillis = ticket.getInTime().getTime();
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+		long outTimeInMillis = ticket.getOutTime().getTime();
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
-            }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
-        }
-    }
+		// TODO: Some tests are failing here. Need to check if this logic is correct
+		/*
+		 * Calcul de la durée de stationnement en minutes en soustrayant le temps de
+		 * sortie du temps d'entrée Et en divisant le résultat par 60 000 pour convertir
+		 * les millisecondes en minutes
+		 */
+		long durationInMinutes = (outTimeInMillis - inTimeInMillis) / 60000;
+		System.out.println("TEST temps duration FareCalculatorService: " + durationInMinutes);
+
+		switch (ticket.getParkingSpot().getParkingType()) {
+		case CAR: {
+			// On divise le tarif horaire par 60 pour obtenir le tarif par minute
+			ticket.setPrice((durationInMinutes * Fare.CAR_RATE_PER_HOUR) / 60);
+			break;
+		}
+		case BIKE: {
+			ticket.setPrice((durationInMinutes * Fare.BIKE_RATE_PER_HOUR) / 60);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unkown Parking Type");
+		}
+	}
 }
