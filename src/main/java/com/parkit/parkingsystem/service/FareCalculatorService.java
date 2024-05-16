c0006fe3646b7ae1e96a651d4ed3dfaa8ad2d3d3
Michelle Mairet <michelle.rk.mairet@gmail.com>
2024-05-16 17:07:56 +0200

    Adding new feature, 5% discount for regular user

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -6,6 +6,10 @@ import com.parkit.parkingsystem.model.Ticket;
 public class FareCalculatorService {
 
 	public void calculateFare(Ticket ticket) {
+		calculateFare(ticket, false);
+	}
+
+	public void calculateFare(Ticket ticket, boolean discount) {
 		if ((ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime()))) {
 			throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
 		}
@@ -16,37 +20,39 @@ public class FareCalculatorService {
 
 		long outTimeInMillis = ticket.getOutTime().getTime();
 
-		// TODO: Some tests are failing here. Need to check if this logic is correct
 		/*
 		 * Calculate parking time in minutes by subtracting the exit time
 		 * (outTimeInMillis) from the entry time (inTimeInMillis) And divide the result
 		 * by 60 000 to convert milliseconds to minutes
 		 */
 		long durationInMinutes = (outTimeInMillis - inTimeInMillis) / 60000;
-		System.out.println("TEST temps duration FareCalculatorService: " + durationInMinutes);
 
+		// fare per hour
+		double ratePerHour;
 		switch (ticket.getParkingSpot().getParkingType()) {
-		case CAR: {
-			// Implements the free fare if parking is less than 30 minutes
-			if (durationInMinutes < 30) {
-				ticket.setPrice(0);
-			} else {
-				// Dividing the fare per hour by 60 to have the fare per minute
-				ticket.setPrice((durationInMinutes * Fare.CAR_RATE_PER_HOUR) / 60);
-			}
+		case CAR:
+			ratePerHour = Fare.CAR_RATE_PER_HOUR;
 			break;
-		}
-		case BIKE: {
-			// Implements the free fare if parking is less than 30 minutes
-			if (durationInMinutes < 30) {
-				ticket.setPrice(0);
-			} else {
-				ticket.setPrice((durationInMinutes * Fare.BIKE_RATE_PER_HOUR) / 60);
-			}
+		case BIKE:
+			ratePerHour = Fare.BIKE_RATE_PER_HOUR;
 			break;
-		}
 		default:
 			throw new IllegalArgumentException("Unkown Parking Type");
 		}
+
+		// total cost of parking
+		double fare = 0;
+
+		if (durationInMinutes < 30) { // free fare for parking lot less than 30 minutes
+			fare = 0;
+		} else {
+			fare = (durationInMinutes * ratePerHour) / 60;
+		}
+
+		if (discount) { // if boolean discount is true (regular user)
+			fare = fare * 0.95; // Regular user gets 5% discount
+		}
+
+		ticket.setPrice(fare);
 	}
 }
